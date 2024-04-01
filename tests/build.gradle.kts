6346518b3eb9c1db61588809e8984787d28108b8
Fred George <fredgeorge@acm.org>
2024-04-01 13:31:11 +0200

    Fred: Updated to latest Kotlin, Gradle, and JUnit

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -22,14 +22,14 @@ dependencies {
     implementation(project(":engine"))
 
     // Align versions of all Kotlin components
-    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.22"))
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.9.23"))
 
     // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.22")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.9.23")
 
     // Jupiter using JUnit 5
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-	testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
 }
 
 tasks.test {
